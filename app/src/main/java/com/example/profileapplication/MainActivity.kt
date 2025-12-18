9ed695d7e37f7f18baa10cca7792ad0feb6b2bfb
egmfabro <egmfabro@gmail.com>
2025-12-19 03:56:25 +0800

    Initial Commit - Android Profile App

---
+++ app/src/main/java/com/example/profileapplication/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.profileapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.widget.Toast
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val followButton = findViewById<Button>(R.id.buttonAction)
+
+        followButton.setOnClickListener {
+            Toast.makeText(this, "Praise the Fool", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
